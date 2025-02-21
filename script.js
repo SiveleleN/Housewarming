d989966e5a85ec2b1b9cd5a1501f8cd96fa0dda3
Sivelele Nkamane <sivelele.nkamane@bountyapparel.co.za>
2025-02-21 09:34:39 +0200

    new

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("rsvpSubmitted")) {
+        document.querySelector(".envelope").classList.add("locked");
+    }
+});
+
+function toggleNote() {
+    var envelope = document.querySelector(".envelope");
+    var note = document.querySelector(".note");
+
+    if (!envelope.classList.contains("locked")) {
+        note.classList.toggle("show-note");
+    }
+}
+
+function updateRSVP(status) {
+    var name = prompt("Enter your name:");
+    if (name) {
+        sendEmail(name, status);
+
+        document.querySelector(".note").classList.remove("show-note");
+        document.querySelector(".envelope").classList.add("locked");
+        alert("Your response has been sent! You cannot RSVP again.");
+
+        localStorage.setItem("rsvpSubmitted", true);
+    }
+}
+
+function sendEmail(name, status) {
+    var email = "sivelelenkamane29@gmail.com"; 
+    var form = document.createElement("form");
+    form.action = "https://formsubmit.co/" + email;
+    form.method = "POST";
+
+    var inputName = document.createElement("input");
+    inputName.type = "hidden";
+    inputName.name = "name";
+    inputName.value = name;
+    form.appendChild(inputName);
+
+    var inputStatus = document.createElement("input");
+    inputStatus.type = "hidden";
+    inputStatus.name = "status";
+    inputStatus.value = status === 'accepted' ? "Accepted" : "Rejected";
+    form.appendChild(inputStatus);
+
+    var inputSubmit = document.createElement("input");
+    inputSubmit.type = "submit";
+    form.appendChild(inputSubmit);
+
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+}
